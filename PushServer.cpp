e0f04f1bf3451bca38a925cb3f7758f660e1dd01
JACKY_LUK <jkluk@sfu.ca>
2016-04-05 16:20:55 -0700

    Add untested PushStatus operation

--- PushServer.cpp
+++ PushServer.cpp
@@ -51,6 +51,53 @@ using web::http::experimental::listener::http_listener;
 using prop_str_vals_t = vector<pair<string,string>>;
 
 constexpr const char* def_url = "http://localhost:34574";
+constexpr const char* addr {"http://localhost:34568/"};
+constexpr const char* auth_addr {"http://localhost:34570/"};
+
+const string read_entity_admin {"ReadEntityAdmin"};
+const string update_entity_admin {"UpdateEntityAdmin"};
+
+const string data_table_name {"DataTable"};
+const string data_table_friends_prop {"Friends"};
+const string data_table_update_prop {"Updates"};
+
+/*
+  Given an HTTP message with a JSON body, return the JSON
+  body as an unordered map of strings to strings.
+
+  Note that all types of JSON values are returned as strings.
+  Use C++ conversion utilities to convert to numbers or dates
+  as necessary.
+ */
+unordered_map<string,string> get_json_body(http_request message) {  
+  unordered_map<string,string> results {};
+  const http_headers& headers {message.headers()};
+  auto content_type (headers.find("Content-Type"));
+  if (content_type == headers.end() ||
+      content_type->second != "application/json")
+    return results;
+
+  value json{};
+  message.extract_json(true)
+    .then([&json](value v) -> bool
+    {
+            json = v;
+      return true;
+    })
+    .wait();
+
+  if (json.is_object()) {
+    for (const auto& v : json.as_object()) {
+      if (v.second.is_string()) {
+  results[v.first] = v.second.as_string();
+      }
+      else {
+  results[v.first] = v.second.serialize();
+      }
+    }
+  }
+  return results;
+}
 
 /*
   Top-level routine for processing all HTTP GET requests.
@@ -65,9 +112,55 @@ void handle_get(http_request message) {
  */
 void handle_post(http_request message) {
   string path {uri::decode(message.relative_uri().path())};
-  cout << endl << "**** PostServer POST " << path << endl;
+  cout << endl << "**** PushServer POST " << path << endl;
   auto paths = uri::split_path(path);
-  message.reply(status_codes::NotImplemented);
+  if (paths.size() != 4 || paths[0] != "PushStatus") {
+    message.reply(status_codes::BadRequest);
+  }
+  assert(paths[0] == "PushStatus");
+  string user_country {paths[1]};
+  string user_name {paths[2]};
+  string status {paths[3]};
+  //Assuming status stored in updates are of the form user_country;user_name;status\n
+  string new_status {user_country + ";" + user_name + ";" + status +"\n"};
+
+  unordered_map<string, string> friend_map {get_json_body(message)};
+  if (friend_map.size() == 1 
+      && friend_map.begin()->first == data_table_friends_prop) {
+    friends_list_t friend_list {parse_friends_list(friend_map.begin()->second)};
+    
+    for (auto user_friend = friend_list.begin(); user_friend != friend_list.end(); ++user_friend) {
+      pair<status_code,value> get_old_updates {do_request (methods::GET,
+                                                           addr + 
+                                                           read_entity_admin + "/" + 
+                                                           data_table_name + "/" + 
+                                                           user_friend->first + "/" +
+                                                           user_friend->second)};
+      if (get_old_updates.first != status_codes::OK) {
+        continue;
+      }
+
+      unordered_map<string,string> user_friend_properties {unpack_json_object(get_old_updates.second)};
+      string updates {user_friend_properties[data_table_friends_prop]};
+
+      updates += new_status;
+
+      pair<status_code,value> update_friend {do_request (methods::PUT,
+                                                         addr + 
+                                                         update_entity_admin + "/" + 
+                                                         data_table_name + "/" + 
+                                                         user_friend->first + "/" +
+                                                         user_friend->second,
+                                                         value::object(vector<pair<string,value>>
+                                                                       {make_pair(data_table_update_prop,
+                                                                                  value::string(updates))}))};
+    }
+    message.reply(status_codes::OK);
+  }
+
+  else {
+    message.reply(status_codes::BadRequest);
+  }
 }
 
 /*
